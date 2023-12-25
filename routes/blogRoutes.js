9fd98acb50d9f2800a58ff0c3d91492bb5f7a5da
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2023-12-25 20:05:48 +0530

    Update blogRoutes.js
    Adding auth middleware

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -8,26 +8,28 @@ const {
   userBlogControlller,
 } = require("../controllers/blogControlller");
 
+const authMiddleware = require("../middleware/auth");
+
 //router object
 const router = express.Router();
 
 //routes
 // GET || all blogs
-router.get("/all-blog", getAllBlogsController);
+router.get("/all-blog",authMiddleware, getAllBlogsController);
 
 //POST || create blog
-router.post("/create-blog", createBlogController);
+router.post("/create-blog", authMiddleware, createBlogController);
 
 //PUT || update blog
-router.put("/update-blog/:id", updateBlogController);
+router.put("/update-blog/:id",authMiddleware, updateBlogController);
 
 //GET || SIngle Blog Details
-router.get("/get-blog/:id", getBlogByIdController);
+router.get("/get-blog/:id",authMiddleware, getBlogByIdController);
 
 //DELETE || delete blog
-router.delete("/delete-blog/:id", deleteBlogController);
+router.delete("/delete-blog/:id",authMiddleware, deleteBlogController);
 
 //GET || user blog
-router.get("/user-blog/:id", userBlogControlller);
+router.get("/user-blog/:id",authMiddleware, userBlogControlller);
 
 module.exports = router;
